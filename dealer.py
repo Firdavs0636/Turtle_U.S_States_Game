d4904dfeeec9262a2cf4144f64f13fcaa08a735d
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-03-01 19:42:22 +0900

    new

--- dealer.py
+++ dealer.py
@@ -5,16 +5,14 @@ class Dealer():
 
     def __init__(self):
         self.data = pandas.read_csv('50_states.csv')
-        self.names = self.data['state'].to_list()
+        self.names = self.data.state.to_list()
         self.x_y = ()
 
     def check(self, guess):
-        id = 0
-        for state in self.names:
+        for i, state in enumerate(self.names):
             if state == guess:
-                x = (self.data['x'].iloc[id])
-                y = (self.data['y'].iloc[id])
+                x = (self.data['x'].iloc[i])
+                y = (self.data['y'].iloc[i])
                 self.x_y = x, y
                 return True
-            id += 1
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import turtle
+
+import pandas
+
 from dealer import Dealer
 
 csv = Dealer()
@@ -20,12 +23,22 @@ downy.hideturtle()
 while game_is_on:
     score = len(record_list)
     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()
-    if csv.check(answer_state):
+    if answer_state == 'Exit':
+        missing_states = [temp for temp in csv.name if temp not in record_list]
+
+        # for temp in csv.names:
+        #     if temp not in record_list:
+        #         missing_states.append(temp)
+
+        remaining_answers = pandas.DataFrame(missing_states)
+        remaining_answers.to_csv('remained_answers.csv')
+        break
+    elif csv.check(answer_state):
         if answer_state not in record_list:
             downy.goto(csv.x_y)
             downy.write(answer_state, True, align="center")
             record_list.append(answer_state)
-        if score == 2:
+        if score == 49:
             game_is_on = False
             game_prompt = screen.textinput(title="YOU WIN ! ! !",
                                            prompt="CONGRATULATIONS You Win The Game! \nWould You Like To Play Again? Type Yes/No").lower()
@@ -34,4 +47,4 @@ while game_is_on:
                 downy.clear()
                 game_is_on = True
 
-screen.exitonclick()
+
